b93cdec0e548334451449529081da3e8417df53f
gwanwoo-you <skymill2000@gmail.com>
2020-06-16 11:34:30 +0900

    javascript basic

--- objectArray.js
+++ objectArray.js
@@ -33,7 +33,11 @@ var car3 = {
 
 //            0     1
 var cars = [car1, car2, car3];
-console.log(cars[0].name);
 
 //#work1 자동차 배열안에 BMW 라는 이름의 차가 있다면 'find !' 라는 문자열 출력
 //for, if 사용
+for(var i = 0; i < cars.length; i ++){
+    if(cars[i].name == "BMW"){
+        console.log('FIND!');
+    }
+}
