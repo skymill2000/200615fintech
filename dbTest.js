855366d1f1ed0a7e34cb6583d8d6eb5877c4d161
gwanwoo-you <skymill2000@gmail.com>
2020-06-16 16:14:15 +0900

    mysql 연동 테스트
    select sql 구문 변경

---
+++ dbTest.js
@@ -0,0 +1,14 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'fintech'
+});
+ 
+connection.connect();
+
+connection.query('SELECT * FROM fintech.user', function (error, results, fields) {
+  if (error) throw error;
+  console.log('모든 회원 정보 : ', results);
+});
